56de9e46a9501cc4b38c7a5978e39b6a8da20385
jay <jayp33233@gmail.com>
2023-01-19 14:11:03 +0530

    prblem- two sum (leetcode)

---
+++ array/two sum/twoSum.java
@@ -0,0 +1,17 @@
+
+public class twoSum {
+    
+    public static void main(String[] args) {
+        int arr[]= {2,3,4,7,5,10};
+        int target = 9;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i]+arr[j] == target) {
+                    System.out.println("["+i+","+j+"]");      //output : [0,3] [2,4]
+                }
+            }
+        }
+    }
+
+}
