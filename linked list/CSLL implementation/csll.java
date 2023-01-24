db88e71c02458b13f81dfa8b98b9acb416d3bba1
jay <jayp33233@gmail.com>
2023-01-24 09:54:57 +0530

    insertion in CSLL(at start , end , specified location)

--- linked list/CSLL implementation/csll.java
+++ linked list/CSLL implementation/csll.java
@@ -7,6 +7,8 @@ class CircularSinglyLinkedList{
    public Node tail;
    public int size;
 
+   // creation of CSLL
+
    public Node createCSLL(int nodeValue){
 
     head = new Node();
@@ -20,6 +22,44 @@ class CircularSinglyLinkedList{
     size=1;
     return head;
    }
+
+   // insertion in CSLL
+
+   public void insertionInCSLL(int nodeValue , int location){
+    
+    Node node = new Node();
+    node.value = nodeValue;
+
+
+    if (head == null) {                                             // if LL not exist
+        createCSLL(nodeValue);
+    }
+    else if (location == 0) {                                       // at the beginning of LL                                  
+        node.next = head;
+        head = node;
+        tail.next = node;
+    }
+    else if (location>=size) {                                       // at the end of LL
+        node.next = tail.next;
+        tail.next = node;
+        tail = node;
+
+    }
+    else{                                                           // at specified location in LL
+        Node currentNode = head;
+        int index = 0;
+
+        while(index<location){
+            currentNode = currentNode.next;
+            index++;
+        }
+
+        Node nexNode = currentNode.next;
+        node.next = nexNode;
+        currentNode.next = node;
+    }
+    size++;
+   }
 }
 
 public class csll {
@@ -28,7 +68,9 @@ public class csll {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
 
         csll.createCSLL(5);
-        System.out.println(csll.head.value);
-        
+        csll.insertionInCSLL(6, 0);
+        csll.insertionInCSLL(7, 2);
+        csll.insertionInCSLL(8, 3);                                 // output : 6 5 7 8
+         
     }
 }
