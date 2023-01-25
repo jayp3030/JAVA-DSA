5702bd0011767218aba0365eea6de82db6e26974
jay <jayp33233@gmail.com>
2023-01-25 08:59:50 +0530

    insertion(at start , end , specified location) in CDLL

--- linked list/CDLL implementation/cdll.java
+++ linked list/CDLL implementation/cdll.java
@@ -8,6 +8,8 @@ class CircularDoublyLinkedList{
     Node tail;
     int size;
 
+    // creation of node in CDLL
+
     public Node creationOfCDLL(int nodeValue){
 
         head = new Node();                                          // step-1
@@ -22,7 +24,46 @@ class CircularDoublyLinkedList{
         size = 1;
 
         return head;
+    }
 
+    // insertion in CDLL
+
+    public void insertionInCDLL(int nodeValue , int location){
+        Node node = new Node();
+        node.value = nodeValue;
+
+        if (head == null) {                                         // if node not exist
+            creationOfCDLL(nodeValue);
+        }
+        else if (location == 0) {                                   // insertion at start
+            node.next = head;
+            node.prev = tail;
+            head.prev = node;
+            head = node;
+            tail.next = node;
+        }
+        else if (location>=size) {                                  // insertion at end
+            node.next = head;
+            node.prev = tail;
+            head.prev = node;
+            tail.next = node;
+            tail = node;
+        }
+        else                                                        // insertion at any position btwn start & end
+        {
+            Node currentNode = head;
+            int index = 0;
+
+            while (index<location-1) {
+                currentNode=currentNode.next;
+                index++;
+            }
+            Node nextNode = currentNode.next;
+            node.prev = currentNode;
+            node.next = nextNode;
+            currentNode.next = node;
+            nextNode.prev = node;
+        }
     }
 }
 
@@ -30,7 +71,9 @@ public class cdll {
     public static void main(String[] args) {
         CircularDoublyLinkedList cdll = new CircularDoublyLinkedList();
 
-        cdll.creationOfCDLL(9);
-        System.out.println(cdll.head.value);
+        cdll.creationOfCDLL(10);
+        cdll.insertionInCDLL(9, 0);
+        cdll.insertionInCDLL(11, 2);
+        cdll.insertionInCDLL(12, 3);
     }
 }
