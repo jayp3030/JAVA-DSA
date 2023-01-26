104f882b6ed63d148e7723108e06adaee4a1a6f1
jay <jayp33233@gmail.com>
2023-01-26 17:12:55 +0530

    enqueue operation in circular queue

--- stack and queue/queue/circular queue(array) implementation/circularQueue.java
+++ stack and queue/queue/circular queue(array) implementation/circularQueue.java
@@ -29,11 +29,35 @@ class CQ{
         if (top+1 == beginningOfQueue) {
             return true;
         } 
-        else if (beginningOfQueue == -1 && top == size-1) {
+        else if (beginningOfQueue == 0 && top == size-1) {
             return true;
         }
         else {
             return false;
+        }
+    }
+
+    // enqueue operation in CQ
+
+    public void enqueue(int value){
+
+        if (isFull()) {
+            System.out.println("CQ is already full");
+        }
+        else if (isEmpty()) {
+            beginningOfQueue = 0;
+            top ++;
+            arr[top] = value;
+            System.out.println(value + " inserted successfully");
+        }
+         else {
+            if (top+1 == size) {
+                top = 0;    
+            }else{
+                top++;
+            }
+            arr[top] = value;
+            System.out.println(value + " inserted successfully");
         }
     }
 }
@@ -42,7 +66,13 @@ public class circularQueue {
         
         CQ cq1 = new CQ(5);                                         // output : CQ is successfully created with size : 5
 
-        System.out.println(cq1.isEmpty());                          // output : true
-        System.out.println(cq1.isFull());                           // output : false
+        cq1.enqueue(10);                                            // output : 10 inserted successfully
+        cq1.enqueue(20);                                            // output : 20 inserted successfully
+        cq1.enqueue(30);                                            // output : 30 inserted successfully
+        cq1.enqueue(40);                                            // output : 40 inserted successfully
+        cq1.enqueue(50);                                            // output : 50 inserted successfully
+
+        System.out.println(cq1.isEmpty());                          // output : false
+        System.out.println(cq1.isFull());                           // output : true
     }
 }
