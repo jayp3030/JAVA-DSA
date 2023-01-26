4b42d5cc4ac5b95250cf52b051e5d780af129975
jay <jayp33233@gmail.com>
2023-01-26 11:41:40 +0530

    isEmpty method in stack

--- stack/stack(array) implementation/arrayStack.java
+++ stack/stack(array) implementation/arrayStack.java
@@ -8,11 +8,21 @@ class Stack{
 
         System.out.println("Stack is created with the size of : "+ size);
     }
+
+    public boolean isEmpty(){
+
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 public class arrayStack {
     public static void main(String[] args) {
 
-        Stack s1 =new Stack(5);
+        Stack s1 =new Stack(5);                                     // output : Stack is created with the size of : 5
 
+        System.out.println(s1.isEmpty());                           // output : true     (there is no element so..)
     }
 }
