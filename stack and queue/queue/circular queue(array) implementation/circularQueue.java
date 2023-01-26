b87f1ebadd622096cf03c4cd99b50a560d257ed4
jay <jayp33233@gmail.com>
2023-01-26 17:01:14 +0530

    isFull and isEmpty method circular queue

--- stack and queue/queue/circular queue(array) implementation/circularQueue.java
+++ stack and queue/queue/circular queue(array) implementation/circularQueue.java
@@ -12,10 +12,37 @@ class CQ{
 
         System.out.println("CQ is successfully created with size : " + size);
     }
+
+    // isEmpty method in CQ
+
+    public boolean isEmpty(){
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // isFull method in CQ
+
+    public boolean isFull(){
+        if (top+1 == beginningOfQueue) {
+            return true;
+        } 
+        else if (beginningOfQueue == -1 && top == size-1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 public class circularQueue {
     public static void main(String[] args) {
         
         CQ cq1 = new CQ(5);                                         // output : CQ is successfully created with size : 5
+
+        System.out.println(cq1.isEmpty());                          // output : true
+        System.out.println(cq1.isFull());                           // output : false
     }
 }
