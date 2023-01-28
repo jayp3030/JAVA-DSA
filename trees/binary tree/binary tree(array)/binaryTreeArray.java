d768a6e0f3766389a912a23e796f8eb69bf0f98e
jay <jayp33233@gmail.com>
2023-01-28 15:44:43 +0530

    insertion of node & isFull method in binary tree

--- trees/binary tree/binary tree(array)/binaryTreeArray.java
+++ trees/binary tree/binary tree(array)/binaryTreeArray.java
@@ -8,10 +8,40 @@ class BinaryTree{
         this.lastUsedIndex = 0;
         System.out.println("blank tree is created with size "+ size);
     }
+
+    // isFUll method
+
+    public boolean isFull(){
+        if (lastUsedIndex == arr.length-1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // insertion of node
+
+    public void insert(String nodeValue){
+        if (!isFull()) {
+            arr[lastUsedIndex+1] = nodeValue;
+            lastUsedIndex++;
+            System.out.println(nodeValue + " inserted successfully");
+        } else {
+            System.out.println("the BT is full");
+        }
+    }
 }
 public class binaryTreeArray {
     public static void main(String[] args) {
 
         BinaryTree bt = new BinaryTree(6);                          // output : blank tree is created with size 6
+
+        bt.insert("N1");                                            // output : N1 inserted successfully
+        bt.insert("N2");                                            // output : N2 inserted successfully
+        bt.insert("N3");                                            // output : N3 inserted successfully
+        bt.insert("N4");                                            // output : N4 inserted successfully
+        bt.insert("N5");                                            // output : N5 inserted successfully
+        bt.insert("N6");                                            // output : N6 inserted successfully
+        bt.insert("N7");                                            // output : the BT is full
     }
 }
