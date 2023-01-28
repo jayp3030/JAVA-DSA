519a6f9cdc313608e594d65791e065839b79ded3
jay <jayp33233@gmail.com>
2023-01-28 09:37:40 +0530

    creation of binary tree

---
+++ trees/binary tree/binary tree(Linked list)/binaryTreeLL.java
@@ -0,0 +1,21 @@
+class BinaryNode{
+
+    public String value;
+    public BinaryNode left;
+    public BinaryNode right;
+    public int height;                                             // useful in other operations
+}
+class BinaryTree{
+    BinaryNode root;
+
+    public BinaryTree(){
+        this.root = null;
+    }
+}
+public class binaryTreeLL {
+    public static void main(String[] args) {
+
+        BinaryTree bt = new BinaryTree();
+        
+    }
+}
