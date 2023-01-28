1852119266b7cc3e9dbb906bb8dbc63b630621e4
jay <jayp33233@gmail.com>
2023-01-28 10:33:00 +0530

    PreOreder traversal(root ,left , right) of binary tree

--- trees/binary tree/binary tree(Linked list)/binaryTreeLL.java
+++ trees/binary tree/binary tree(Linked list)/binaryTreeLL.java
@@ -11,11 +11,23 @@ class BinaryTree{
     public BinaryTree(){
         this.root = null;
     }
+
+    // PreOrder traversal
+
+    public void PreOrder(BinaryNode node){
+
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.value + " ");                       // root
+        PreOrder(node.left);                                        // left subtree
+        PreOrder(node.right);                                       // rigth subtree
+    }
 }
 public class binaryTreeLL {
     public static void main(String[] args) {
 
         BinaryTree bt = new BinaryTree();
-        
+
     }
 }
