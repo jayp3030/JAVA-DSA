e2a5ee1fd773fb364434b1e26be4c491de52ad80
jay <jayp33233@gmail.com>
2023-01-29 09:41:00 +0530

    insertion in binary tree

--- trees/binary search tree/BST implementation/binarySearchTree.java
+++ trees/binary search tree/BST implementation/binarySearchTree.java
@@ -5,16 +5,46 @@ class BinaryNode{
     public int height;
 }
 class BST{
+
     BinaryNode root;
 
     public BST(){
         this.root = null;
         System.out.println("BST is created with null root node");
     }
+
+    // insertion in BST
+
+    private BinaryNode insertion(BinaryNode currentNode , int value){
+
+        if (currentNode== null) {
+            BinaryNode node = new BinaryNode();
+            node.value = value;
+            System.out.println(value + " successfully inserted");
+            return currentNode;
+        } else if (value <= currentNode.value) {
+            currentNode.left = insertion(currentNode.left, value);
+            return currentNode;
+        } else{
+           currentNode.right = insertion(currentNode.right, value);
+           return currentNode; 
+        }
+    }
+
+    public void insert(int nodeValue){
+        insertion(root, nodeValue);
+    }
 }
 public class binarySearchTree {
     public static void main(String[] args) {
 
         BST bst = new BST();                                        // output : BST is created with null root node
+
+        bst.insert(60);                                             // output : 60 successfully inserted
+        bst.insert(20);                                             // output : 20 successfully inserted
+        bst.insert(30);                                             // output : 30 successfully inserted
+        bst.insert(80);                                             // output : 80 successfully inserted
+        bst.insert(10);                                             // output : 10 successfully inserted
+        bst.insert(70);                                             // output : 70 successfully inserted
     }
 }
