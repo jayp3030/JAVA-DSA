d7fee608e85c0d9ab7f6016174e5183362dd82c9
jay <jayp33233@gmail.com>
2023-01-31 11:49:46 +0530

    insertion in TRIE tree

--- trees/TRIE/trie implementation/trieTree.java
+++ trees/TRIE/trie implementation/trieTree.java
@@ -17,9 +17,33 @@ class Trie{
         root = new TrieNode();
         System.out.println("Trie is created successfully");
     }
+
+    // insertion in trie
+
+    public void insert(String word) {
+
+        TrieNode current = root;
+
+        for (int i=0; i<word.length(); i++) {
+          char ch = word.charAt(i);
+          TrieNode node = current.children.get(ch);
+          if (node == null) {
+            node = new TrieNode();
+            current.children.put(ch, node);
+          }
+          current = node;
+        }
+        current.endOfString = true;
+        System.out.println("Successfully inserted " + word+ " in Trie");
+
+      }
 }
 public class trieTree {
     public static void main(String[] args) {
         Trie t = new Trie();                                        // output : Trie is created successfully
+
+        t.insert("API");                                            // output : Successfully inserted API in Trie
+        t.insert("APIS");                                            // output : Successfully inserted APIS in Trie
+        
     }
 }
