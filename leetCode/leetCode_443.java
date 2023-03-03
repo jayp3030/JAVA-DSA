a1debe14349a7051b634a0c72aaf2b2e6222c0df
jay <jayp33233@gmail.com>
2023-03-03 16:36:37 +0530

    leetCode_443 - String Compression

---
+++ leetCode/leetCode_443.java
@@ -0,0 +1,55 @@
+// Given an array of characters chars, compress it using the following algorithm:
+// Begin with an empty string s. For each group of consecutive repeating characters in chars:
+// If the group's length is 1, append the character to s.
+// Otherwise, append the character followed by the group's length.
+// The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+// After you are done modifying the input array, return the new length of the array.
+// You must write an algorithm that uses only constant extra space.
+
+ 
+// Example 1:
+// Input: chars = ["a","a","b","b","c","c","c"]
+// Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+// Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+
+// Example 2:
+// Input: chars = ["a"]
+// Output: Return 1, and the first character of the input array should be: ["a"]
+// Explanation: The only group is "a", which remains uncompressed since it's a single character.
+
+// Example 3:
+// Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+// Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+// Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+
+class Solution {
+    public int compress(char[] chars) {
+
+        if(chars.length == 1){
+            return 1;
+        }
+
+        int i = 0; // keep track to modify array
+
+        // j = 1 for starting from index one and to iterate array..
+        // count = 1 will be keep track of number of occurrences of char
+        for(int j = 1,count = 1 ; j <= chars.length ; j++,count++ ){
+            // if j reach to end of array OR curr is not equal prev
+            if(j == chars.length || chars[j] != chars[j-1]){
+                chars[i] = chars[j-1];
+                i++;
+                // if count == 1 then we dont add count to array
+                if(count > 1){
+                    // testcase 3 - ab12 - so we have to convert count to string and add each char in array
+                    String s = String.valueOf(count);
+                    for(int k = 0 ; k < s.length() ;k++){
+                        chars[i] = s.charAt(k);
+                        i++;
+                    }
+                }
+                count = 0;
+            }
+        }
+        return i;
+    }
+}
