411a99ab2f9327421f1c76f19a23144ea95dc7c7
jay <jayp33233@gmail.com>
2023-03-08 17:52:12 +0530

    leetCode_875 - koko eating banana

---
+++ leetCode/leetCode_875.java
@@ -0,0 +1,44 @@
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+// Return the minimum integer k such that she can eat all the bananas within h hours.
+
+// Example 1:
+// Input: piles = [3,6,7,11], h = 8
+// Output: 4
+
+// Example 2:
+// Input: piles = [30,11,23,4,20], h = 5
+// Output: 30
+
+// Example 3:
+// Input: piles = [30,11,23,4,20], h = 6
+// Output: 23
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 0;
+
+        for(int i = 0 ; i < piles.length ; i++){
+            right = Math.max(piles[i],right);     // right = max of piles in array
+        }
+
+        while( left < right){
+            int mid = (left+right)/2;
+            if(canEatAll(piles,mid,h)) 
+                right = mid;
+            else
+                left = mid+1;
+        }
+        return left;    // always minimum time
+    }
+
+    private boolean canEatAll(int[] piles , int speed , int h){
+         int time = 0;
+        for (int pile : piles) {
+            time += (pile+speed- 1) / speed ;
+        }
+        return time <= h;
+    }
+}
