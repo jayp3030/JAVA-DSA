c09378403a79c94f1b6d367a67bdd2f8740c7a01
jay <jayp33233@gmail.com>
2023-03-14 18:04:18 +0530

    leetCode_112 - path sum

--- leetCode/leetCode_112.java
+++ leetCode/leetCode_112.java
@@ -49,3 +49,23 @@ class Solution {
     }
  
 }
+
+
+// ------------------------------------- alternative -------------------------------------------- 
+
+class Solution {
+    public boolean rootToLeafPathSum(TreeNode root, int targetSum, int sum){
+        if(root == null)
+            return false;
+        if(root.left == null && root.right == null){
+            sum = sum + root.val;
+            if(sum == targetSum)
+                return true;   
+        }
+        return rootToLeafPathSum(root.left, targetSum, sum + root.val) || rootToLeafPathSum(root.right, targetSum, sum + root.val);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        int sum = 0;
+        return rootToLeafPathSum(root, targetSum, sum);
+    }
+}
