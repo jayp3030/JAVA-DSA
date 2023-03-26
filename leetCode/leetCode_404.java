5a385fe962a588b7ec9631b2b028712621565620
jay <jayp33233@gmail.com>
2023-03-26 17:23:11 +0530

    leetCode_404 - sum of left leaves

---
+++ leetCode/leetCode_404.java
@@ -0,0 +1,49 @@
+// Given the root of a binary tree, return the sum of all left leaves.
+
+// A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 24
+// Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+
+// Example 2:
+// Input: root = [1]
+// Output: 0
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null ) return 0;
+        int sum = 0;
+
+        if( root.left != null){
+          // check for leaves
+          if(root.left.left == null && root.left.right == null){
+            sum += root.left.val;
+          }
+          else{
+            sum += sumOfLeftLeaves(root.left);
+          }
+        }
+        // same for right sub tree
+        sum += sumOfLeftLeaves(root.right);
+
+        return sum;
+    }
+}
