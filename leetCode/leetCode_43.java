2ac5baf6c66c660a92cbef2fc120107c9d5c5a28
jay <jayp33233@gmail.com>
2023-04-05 08:37:02 +0530

    leetCode_43 - multiply string

---
+++ leetCode/leetCode_43.java
@@ -0,0 +1,53 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+// Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+// Example 1:
+
+// Input: num1 = "2", num2 = "3"
+// Output: "6"
+
+// Example 2:
+
+// Input: num1 = "123", num2 = "456"
+// Output: "56088"
+ 
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        int m = num1.length();
+        int n = num2.length();
+
+        // Base Conditions
+        if (m == 0 || n == 0 || "0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+        // Result can be maximum of length M + N.
+        // For example 99 * 99 = 9801 (Result is of length 4)
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                // Adding previous values in result array into this product.
+                product += result[i + j + 1];
+
+                // Adding the new product into the result array
+                result[i + j + 1] = product % 10;
+                result[i + j] += product / 10;
+            }
+        }
+
+        // Generating the result String
+        StringBuilder sb = new StringBuilder();
+        for (int r : result) {
+            // Ignoring leading zeros
+            if (sb.length() == 0 && r == 0) {
+                continue;
+            }
+            sb.append(r);
+        }
+
+        return sb.toString();
+    }
+}
