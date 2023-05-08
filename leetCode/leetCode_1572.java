399afc44ad2f014516fb0c21fc89e4af987e0349
jay <jayp33233@gmail.com>
2023-05-08 09:36:35 +0530

    leetCode_1572 - matrix diagonal sum

---
+++ leetCode/leetCode_1572.java
@@ -0,0 +1,33 @@
+// Given a square matrix mat, return the sum of the matrix diagonals.
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+// Example 1:
+// Input: mat = [[1,2,3],
+//               [4,5,6],
+//               [7,8,9]]
+// Output: 25
+// Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+// Notice that element mat[1][1] = 5 is counted only once.
+
+// Example 2:
+// Input: mat = [[1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1]]
+// Output: 8
+
+// Example 3:
+// Input: mat = [[5]]
+// Output: 5
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for(int i=0, end=mat.length -1; i<mat.length; i++, end--) {
+            sum += mat[i][i] + mat[i][end];
+            if (i == end)
+                sum -= mat[i][end];
+        }
+        return sum;
+    }
+}
