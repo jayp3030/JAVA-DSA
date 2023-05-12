03fd96c394e9ca38662ccf580ecbe812770e850f
jay <jayp33233@gmail.com>
2023-05-12 08:32:53 +0530

    leetcode_110 - balanced binary tree

---
+++ leetCode/leetCode_110.java
@@ -0,0 +1,48 @@
+// Given a binary tree, determine if it is 
+// height-balanced.
+
+//  Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+// Example 3:
+
+// Input: root = []
+// Output: true;
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+        // If the tree is empty, we can say it’s balanced...
+        if (root == null)  return true;
+
+        // Height Function will return -1, when it’s an unbalanced tree...
+		if (Height(root) == -1)  return false;
+        
+		return true;
+	}
+
+    // Create a function to return the “height” of a current subtree using recursion...
+	public int Height(TreeNode root) {
+        // Base case...
+		if (root == null)  return 0;
+
+        // Height of left subtree...
+		int leftHeight = Height(root.left);
+        // Height of height subtree...
+		int rightHight = Height(root.right);
+
+        // In case of left subtree or right subtree unbalanced, return -1...
+		if (leftHeight == -1 || rightHight == -1)  return -1;
+        // If their heights differ by more than ‘1’, return -1...
+
+        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+
+
+		return Math.max(leftHeight, rightHight) + 1;
+    }
+}
