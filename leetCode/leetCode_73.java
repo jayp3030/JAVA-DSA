e740a48e3f1ba20bdf906bacb91bb50c89ae3fdd
jay <jayp33233@gmail.com>
2023-06-29 09:31:07 +0530

    leetCode_73 - Set matrix zeros

---
+++ leetCode/leetCode_73.java
@@ -0,0 +1,38 @@
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+// You must do it in place.
+
+// Example 1:
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+// Example 2:
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;      // rows
+        int n = matrix[0].length;    // cols
+
+        // temp arrays to keep track of 0's
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = -1;
+                    col[j] = -1;
+                }
+            }
+        }
+
+        for( int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(row[i] == -1 || col[j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
